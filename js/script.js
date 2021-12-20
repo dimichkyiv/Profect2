5cfcbf3f7732e10907bdf45baee672f3e2454154
dimichkyiv <dimichmf1995@gmail.com>
2021-12-20 13:03:44 +0200

    Page actions

--- js/script.js
+++ js/script.js
@@ -24,3 +24,23 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img');
+const poster = document.querySelector('.promo__bg');
+const genre = poster.querySelector('.promo__genre');
+const movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+genre.textContent = 'драма';
+poster.style.backgroundImage = "url('img/bg.jpg')";
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+movieDB.movies.forEach((item, i) =>{
+    movieList.innerHTML += `
+    <li class="promo__interactive-item"> ${i+1} ${item}
+    <div class="delete"></div></li>
+    `;
+});
